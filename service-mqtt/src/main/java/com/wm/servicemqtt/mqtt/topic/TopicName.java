d322d0d596338219350d563370bacf8d2a85f7e4
WangMin <wangmin@123>
2022-03-11 09:35:43 +0800

    gateway

--- service-mqtt/src/main/java/com/wm/servicemqtt/mqtt/topic/TopicName.java
+++ service-mqtt/src/main/java/com/wm/servicemqtt/mqtt/topic/TopicName.java
@@ -1,20 +1,26 @@
 package com.wm.servicemqtt.mqtt.topic;
 
+/**
+ * 订阅的topic
+ */
 public enum TopicName {
 
-    DEFAULT_CONSUME_TOPIC(1,"from/device/topic/patrol/#"),
-    DEFAULT_PRODUCER_TOPIC(2,"send/device/topic/"),
-    DEFAULT_WILL_TOPIC(3,"send/device/connect/lost"),
-    DEVICE_ONLINE(4,"/device/online/#"),
-    DEVICE_OFFLINE(5,"/device/offline/#");
+    DEFAULT_CONSUME_TOPIC(1,"from/device/topic/patrol/#","test1"),
+    DEFAULT_PRODUCER_TOPIC(2,"send/device/topic/","哈哈"),
+    DEFAULT_WILL_TOPIC(3,"send/device/connect/lost","xxx"),
+    DEVICE_ONLINE(4,"/device/online/#","xxx"),
+    DEVICE_aaa(5,"/t/#","xxx"),
+    DEVICE_OFFLINE(6,"/device/offline/#","xxx");
 
 
     private final Integer key;
     private final String value;
+    private final String desc;
 
-    private TopicName(Integer key,String value){
+    private TopicName(Integer key,String value,String desc){
         this.key = key;
         this.value = value;
+        this.desc = desc;
     }
     public Integer getKey() {
         return key;
@@ -22,6 +28,7 @@ public enum TopicName {
     public String getValue() {
         return value;
     }
+    public String getDesc(){return desc;}
 
     @Override
     public String toString() {
