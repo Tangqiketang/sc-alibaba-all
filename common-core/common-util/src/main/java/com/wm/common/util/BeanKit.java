a8f1159b0a025f99e3a8d5d09c28a379e8ec7ee9
WangMin <wangmin@123>
2022-08-16 16:33:50 +0800

    工具类

--- common-core/common-util/src/main/java/com/wm/common/util/BeanKit.java
+++ common-core/common-util/src/main/java/com/wm/common/util/BeanKit.java
@@ -1,5 +1,12 @@
 package com.wm.common.util;
 
+import com.alibaba.fastjson.JSON;
+import com.rits.cloning.Cloner;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 描述:
  *
@@ -7,4 +14,77 @@ package com.wm.common.util;
  * @create 2022-08-05 18:41
  */
 public class BeanKit {
+
+    private static final Cloner cloner = new Cloner();
+
+    /**
+     * 复制对象（深度拷贝）
+     * @param object
+     * @param <T>
+     * @return
+     */
+    public static <T> T clone(final T object){
+        if (object == null) {
+            return null;
+        }
+        return cloner.deepClone(object);
+    }
+
+    /**
+     * 复制集合（深度拷贝）
+     * @param object
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> cloneList(final List<T> object){
+        if (object == null) {
+            return null;
+        }
+        return cloner.deepClone(object);
+    }
+
+    public static<T> T deepClone(T src) throws IOException, ClassNotFoundException {
+        Object obj = null;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+        objectOutputStream.writeObject(src);
+        objectOutputStream.close();
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+        ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+        obj = objectInputStream.readObject();
+        objectInputStream.close();
+        return (T) obj;
+    }
+
+
+    /**
+     * 复制对象到指定类（深度拷贝）
+     * @param object
+     * @param destclas 指定类
+     * @param <T>
+     * @return
+     */
+    public static <T> T clone(final Object object, Class<T> destclas){
+        if (object == null) {
+            return null;
+        }
+        String json = JSON.toJSONString(object);
+        return JSON.parseObject(json, destclas);
+    }
+
+    /**
+     * 复制集合到指定类（深度拷贝）
+     * @param object
+     * @param destclas 指定类
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> cloneList(List<?> object, Class<T> destclas) {
+        if (object == null) {
+            return new ArrayList<T>();
+        }
+        String json = JSON.toJSONString(object);
+        return JSON.parseArray(json, destclas);
+    }
+
 }
--- common-core/common-util/src/main/java/com/wm/common/util/CollectionKit.java
+++ common-core/common-util/src/main/java/com/wm/common/util/CollectionKit.java
@@ -1,9 +1,13 @@
 package com.wm.common.util;
 
+import com.alibaba.fastjson.JSON;
+import lombok.Data;
 import org.springframework.util.CollectionUtils;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 描述:
@@ -13,26 +17,43 @@ import java.util.List;
  */
 public class CollectionKit {
 
-
     /**
-     *  Array 中的元素都添加到 List/Set 中
-     * @param array
-     * @param collection
-     * @param <E>
+     * 返回交集。取2个集合中，相同的部分。不影响原来的集合内容
+     * @param list2
+     * @return
      */
-   public static <E> void mergeArrayIntoCollection(Object array, Collection<E> collection){
-        CollectionUtils.mergeArrayIntoCollection(array,collection);
-   }
+    public static List<String> retainAll(List<String> list1,List<String> list2){
+        List<String> result = new ArrayList<>(list1);
+        result.retainAll(list2);
+        return result;
+
+    }
 
     /**
-     * 返回list中最后一个元素
-     * @param list
+     * 把list1中去掉list2,不影响原来的集合内容
+     * @param list1 [1,2,3]
+     * @param list2 [1,5]
+     * @return [2,3]    (23要删的| 1 |5要插入的)
+     */
+    public static List<String> remove(List<String> list1,List<String> list2){
+        List<String> result = new ArrayList<>(list1);
+        if(!CollectionUtils.isEmpty(list2)){
+            result.remove(list2);
+        }
+        return result;
+    }
+
+
+    /**
+     * 复制集合。(浅拷贝,新集合,元素)
+     * @param source
      * @param <T>
      * @return
      */
-   public static <T> T lastElement(List<T> list){
-       return CollectionUtils.lastElement(list);
-   }
+    public static <T> List<T> newCopy(List<T> source){
+        return new ArrayList<>(source);
+    }
+
 
 
 }
