9eb7fd56e0bf438562e0958d535d3ff7810adfb3
WangMin <wangmin@123>
2022-09-20 15:30:08 +0800

    时间段合并

--- common-core/common-util/src/main/java/com/wm/common/config/threadpool/CustomThreadPoolTaskExecutor.java
+++ common-core/common-util/src/main/java/com/wm/common/config/threadpool/CustomThreadPoolTaskExecutor.java
@@ -2,6 +2,7 @@ package com.wm.common.config.threadpool;
 
 import com.alibaba.ttl.TtlCallable;
 import com.alibaba.ttl.TtlRunnable;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.util.concurrent.ListenableFuture;
 
@@ -14,12 +15,24 @@ import java.util.concurrent.Future;
  * @author wangmin
  * @date 2019/8/14
  */
+@Slf4j
 public class CustomThreadPoolTaskExecutor extends ThreadPoolTaskExecutor {
     private static final long serialVersionUID = -5887035957049288777L;
 
+
+
+    /***********************实现线程池监控**********************/
     @Override
     public void execute(Runnable runnable) {
         Runnable ttlRunnable = TtlRunnable.get(runnable);
+        log.debug("正在工作的线程数:{},当前存在的线程数:{},历史最大线程数:{},已完成的任务数:{},队列中任务数量:{}",
+                getThreadPoolExecutor().getActiveCount(),
+                getThreadPoolExecutor().getPoolSize(),
+                getThreadPoolExecutor().getLargestPoolSize(),
+                getThreadPoolExecutor().getCompletedTaskCount(),
+                getThreadPoolExecutor().getQueue().size()
+                );
+        ;
         super.execute(ttlRunnable);
     }
 
