a77fee457eed2edc6775572e60fca35aef5075e7
Tangqiketang <641718840@qq.com>
2023-01-10 12:47:03 +0800

    增加mongodb常用curd聚合测试类

---
+++ common-core/common-mongo/src/main/java/com/wm/mongo/entity/User.java
@@ -0,0 +1,59 @@
+package com.wm.mongo.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 描述:
+ *
+ * @auther WangMin
+ * @create 2023-01-10 11:16
+ */
+@Document(collection  = "user")
+@Data
+public class User implements Serializable {
+
+    @ApiModelProperty(value = "自动生成主键")
+    @Id
+    private String id;
+
+    //@Indexed
+    @ApiModelProperty(value = "用户名")
+    @Field("username")
+    private String username;
+
+    @ApiModelProperty(value = "年龄")
+    private Integer age;
+
+    @ApiModelProperty(value = "性别")
+    private Integer gender;
+
+    @ApiModelProperty(value = "经度")
+    private Double lng;
+
+    @ApiModelProperty(value = "纬度")
+    private Double lat;
+
+    @ApiModelProperty(value = "高度")
+    private Double height;
+
+    //@Indexed
+    @ApiModelProperty("上报时间")
+    @JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date reportTimestamp;
+
+
+
+
+
+
+}
--- service-web/src/test/java/com/wm/web/MongoOpTest.java
+++ service-web/src/test/java/com/wm/web/MongoOpTest.java
@@ -2,22 +2,21 @@ package com.wm.web;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.wm.mongo.entity.User;
 import org.junit.Test;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.aggregation.Aggregation;
-import org.springframework.data.mongodb.core.aggregation.AggregationResults;
-import org.springframework.data.mongodb.core.aggregation.GroupOperation;
-import org.springframework.data.mongodb.core.aggregation.MatchOperation;
+import org.springframework.data.mongodb.core.aggregation.*;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.repository.support.PageableExecutionUtils;
 
 import javax.annotation.Resource;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -32,12 +31,21 @@ public class MongoOpTest extends BaseTest{
     private MongoTemplate mongoTemplate;
 
     @Test
+    public void simpleInsert(){
+        User user = new User();user.setUsername("用户名399");
+        user.setAge(390);user.setGender(1);
+        user.setReportTimestamp(new Date());user.setLng(120.0101);
+        mongoTemplate.insert(user,"user");
+    }
+
+    //使用对象的方式返回，id主键会自动转成整个string
+    @Test
     public void queryByCriteria(){
-        //等同于 where gender=1 and age<=20 and (username="用户名2" or gender=1) order by username
+        //等同于 where gender=1 and 10<=age<=20 and (username="用户名2" or gender=1) order by username
         Query query = new Query();
         Criteria criteria = new Criteria();
         criteria.and("gender").is(1);
-        criteria.and("age").lte(20);
+        criteria.and("age").lte(20).gte(10);
         criteria.orOperator(Criteria.where("username").is("用户名2"),Criteria.where("gender").is(1));
         //criteria.andOperator(Criteria.where("startTime").lt(now),Criteria.where("endTime").gt(now));
         //Pattern pattern = Pattern.compile("^.*" + eventRuleName + ".*$", Pattern.CASE_INSENSITIVE);
@@ -46,7 +54,7 @@ public class MongoOpTest extends BaseTest{
         //排序
         query.with(Sort.by("username"));
 
-        List<JSONObject> result = mongoTemplate.find(query, JSONObject.class,"user");
+        List<User> result = mongoTemplate.find(query, User.class,"user");
         System.out.println(JSON.toJSONString(result));
     }
 
@@ -70,20 +78,29 @@ public class MongoOpTest extends BaseTest{
     public void queryById(){
         JSONObject result = mongoTemplate.findById("63b8028468f87570e3fbd366",JSONObject.class,"user");
         System.out.println(JSON.toJSONString(result));
-        //id的问题 todo
     }
 
 
     @Test
     public void aggregate(){
         // 管道 先过滤条件,根据性别进行分组统计，结果为个数和总值
-        //db.user.aggregate([{$group:{_id:"$gender",count:{$sum:1},sum:{$sum:"$gender"}}}])
-        //select gender as _id,count(*) as count,sum(gender) as sum from user group by gender; 注意_id是固定写法
-        MatchOperation matchOperation = Aggregation.match(Criteria.where("gender").in(Arrays.asList(0,1,2)));
-        GroupOperation groupOperation = Aggregation.group("gender").count().as("count").sum("gender").as("sum");
-        Aggregation aggregation = Aggregation.newAggregation(matchOperation,groupOperation);
+        //db.user.aggregate([{"$match":{"gender":{"$in":[0,1,2]}}},{"$group":{"_id":"$gender","count":{"$sum":1},"sum":{"$sum":"$gender"}}}])
+        //select gender as _id,count(*) as count,sum(gender) as sum from user where gender in (0,1,2) group by gender; 注意_id是固定写法
+        MatchOperation match = Aggregation.match(Criteria.where("gender")
+                                          .in(Arrays.asList(0,1,2)));
+        GroupOperation group = Aggregation.group("gender")
+                                           .count().as("count")
+                                           .sum("gender").as("sum")
+                                           .push(Aggregation.CURRENT).as("detailRecord"); //把包含的数据也放到查询里
+        ProjectionOperation projection = Aggregation.project("count","sum","detailRecord")
+                                                    .andExclude(Fields.UNDERSCORE_ID)
+                                                    .and(ArrayOperators.Slice.sliceArrayOf("$detailRecord").offset(1).itemCount(20)).as("detailRecord");
+
+        Aggregation aggregation = Aggregation.newAggregation(match,group,projection);
 
         AggregationResults<JSONObject> result = mongoTemplate.aggregate(aggregation, "user", JSONObject.class);
         System.out.println("query: " + aggregation + " | groupQuery " + result.getMappedResults());
     }
+
+    //insert update 事务
 }
